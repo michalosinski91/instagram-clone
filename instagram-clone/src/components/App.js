c87ff53c551811c58c5e893c78a729f967e1fc61
michalosinski91 <mosinski@hotmail.co.uk>
2019-09-24 12:01:26 +0100

    Auth0 login functionality added

--- instagram-clone/src/components/App.js
+++ instagram-clone/src/components/App.js
@@ -1,14 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/App.css';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { useAuth0 } from '../auth/react-auth0-wrapper'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
+import { setContext } from 'apollo-link-context'
+import { Switch, Route } from 'react-router-dom'
 
 import Header from './Header'
 
 function App() {
+  const [accessToken, setAccessToken] = useState('')
+  
+  const { getTokenSilently, loading } = useAuth0()
+
+  //attempts to fetch a token from Auth0 client
+  //if the token is invalid, it is refreshed silently and returned
+  //accessToken gets set
+  const getAccessToken = async () => {
+    try {
+      const token = await getTokenSilently()
+      setAccessToken(token)
+      console.log(token)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  getAccessToken()
+  
+
+
+  const httpLink = new HttpLink({
+    uri: 'https://instaclone-graphql.herokuapp.com/v1/graphql'
+  })
+
+  const authLink = setContext((_, { headers }) => {
+    const token = accessToken
+    if (token) {
+      return {
+        headers: {
+          ...headers,
+          authorization: `bearer ${token}`
+        }
+      }
+    } else {
+      return {
+        headers: {
+          ...headers
+        }
+      }
+    }
+  })
+
+  const client = new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache()
+  })
+
+
+  if (loading) {
+    return 'Loading...'
+  }
+
   return (
-    <Router>
-      <Header />
-    </Router>
+    <ApolloProvider client={client}>
+      <Header />        
+    </ApolloProvider>
   );
 }
 
--- instagram-clone/src/index.js
+++ instagram-clone/src/index.js
@@ -3,24 +3,36 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from '@apollo/react-hooks'
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
-import { setContext } from 'apollo-link-context'
-
-const httpLink = new HttpLink({
-    uri: 'https://instaclone-graphql.herokuapp.com/v1/graphql'
-})
-
-const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache()
-})
+import { Auth0Provider } from './auth/react-auth0-wrapper'
+import config from './auth/auth_config.json'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
-ReactDOM.render(<ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>, document.getElementById('root'));
+//redirects the user to the page they were trying to access prior to logging in
+const onRedirectCallback = appState => {
+    window.history.replaceState(
+        {},
+        document.title,
+        appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname
+    )
+}
+
+
+ReactDOM.render(
+    <Router>
+        <Auth0Provider
+            domain={config.domain}
+            client_id={config.clientId}
+            audience={config.audience}
+            redirect_uri={config.redirect_uri}
+            onRedirectCallback={onRedirectCallback}
+        >
+            <App />
+        </Auth0Provider>
+    </Router>, 
+    document.getElementById('root')
+);
     
-
-
 serviceWorker.unregister();
